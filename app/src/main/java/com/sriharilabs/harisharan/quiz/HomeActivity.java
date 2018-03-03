9e31ac446a900f8e1d0306937f33a5e8e7cfec80
VinitYadav <vinityadav1991@gmail.com>
2018-03-03 16:37:07 +0530

    Change in home activity

--- app/src/main/java/com/sriharilabs/harisharan/quiz/HomeActivity.java
+++ app/src/main/java/com/sriharilabs/harisharan/quiz/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.sriharilabs.harisharan.quiz;
 
+import android.app.Activity;
+import android.database.DatabaseUtils;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sriharilabs.harisharan.quiz.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+        mBinding.setActivity(this);
     }
 }
